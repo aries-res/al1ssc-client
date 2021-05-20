d13d313a329b949cea22971bb4814d1efb8053b9
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-05-20 13:43:44 +0530

    Added isro & aries logo in banner

--- src/Home/Banner0.jsx
+++ src/Home/Banner0.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Button } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import QueueAnim from 'rc-queue-anim';
-import TweenOne from 'rc-tween-one';
-import { isImg } from './utils';
+import React from "react";
+import { Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import QueueAnim from "rc-queue-anim";
+import TweenOne from "rc-tween-one";
+import { isImg } from "./utils";
 
 class Banner extends React.PureComponent {
   render() {
@@ -15,28 +15,35 @@ class Banner extends React.PureComponent {
       <div {...currentProps} {...dataSource.wrapper}>
         <QueueAnim
           key="QueueAnim"
-          type={['bottom', 'top']}
+          type={["bottom", "top"]}
           delay={200}
           {...dataSource.textWrapper}
         >
+          <div className="banner0-byline">
+            A joint effort of
+            <br />
+            <img src="isro_logo.png" alt="isro_logo" />
+            &nbsp;&nbsp;&nbsp;
+            <img src="aries_logo.png" alt="aries_logo" className="aries" />
+          </div>
           <div key="title" {...dataSource.title}>
-            {typeof dataSource.title.children === 'string' &&
+            {typeof dataSource.title.children === "string" &&
             dataSource.title.children.match(isImg) ? (
               <img src={dataSource.title.children} width="100%" alt="img" />
             ) : (
               dataSource.title.children
             )}
           </div>
-          <div key="content" {...dataSource.content}>
+          {/* <div key="content" {...dataSource.content}>
             {dataSource.content.children}
-          </div>
+          </div> */}
           <Button ghost key="button" {...dataSource.button}>
             {dataSource.button.children}
           </Button>
         </QueueAnim>
         <TweenOne
           animation={{
-            y: '-=20',
+            y: "-=20",
             yoyo: true,
             repeat: -1,
             duration: 1000,
