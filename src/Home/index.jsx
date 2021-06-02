26124c25c247d8b764f7d80711b95dcedf53adaa
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-06-02 23:53:54 +0530

    Converted Home from functional to class component

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,6 +1,6 @@
 /* eslint no-undef: 0 */
 /* eslint arrow-parens: 0 */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { enquireScreen } from "enquire-js";
 
 import Nav0 from "./Nav0";
@@ -24,86 +24,66 @@ import {
 } from "./data.source";
 import "./less/antMotionStyle.less";
 
-let isMobile;
-enquireScreen((b) => {
-  isMobile = b;
-});
+export default function Home(props) {
+  // Initial value doesn't actually matter since it will be overridden by useEffect
+  const [isMobile, setIsMobile] = useState(false);
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMobile,
-    };
-  }
-
-  componentDidMount() {
-    // Adapt to mobile phone screen;
+  // Adapt to mobile phone screen;
+  useEffect(() => {
     enquireScreen((b) => {
-      this.setState({ isMobile: !!b });
+      setIsMobile(!!b); //match: b is true, unmatch: b is undefined hence !!b = false
     });
-  }
+  });
 
-  render() {
-    const children = [
-      <Nav0
-        id="Nav0_0"
-        key="Nav0_0"
-        dataSource={Nav00DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Banner0
-        id="Banner0_1"
-        key="Banner0_1"
-        dataSource={Banner01DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      // <Content0
-      //   id="Content0_0"
-      //   key="Content0_0"
-      //   dataSource={Content00DataSource}
-      //   isMobile={this.state.isMobile}
-      // />,
-      <Feature2
-        id="Feature2_0"
-        key="Feature2_0"
-        dataSource={Feature20DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content3
-        id="Content3_0"
-        key="Content3_0"
-        dataSource={Content30DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Feature7
-        id="Feature7_0"
-        key="Feature7_0"
-        dataSource={Feature70DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content5
-        id="Content5_0"
-        key="Content5_0"
-        dataSource={Content50DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Footer1
-        id="Footer1_0"
-        key="Footer1_0"
-        dataSource={Footer10DataSource}
-        isMobile={this.state.isMobile}
-      />,
-    ];
-    return (
-      <div
-        className="templates-wrapper"
-        ref={(d) => {
-          this.dom = d;
-        }}
-      >
-        {children}
-      </div>
-    );
-  }
+  const children = [
+    <Nav0
+      id="Nav0_0"
+      key="Nav0_0"
+      dataSource={Nav00DataSource}
+      isMobile={isMobile}
+    />,
+    <Banner0
+      id="Banner0_1"
+      key="Banner0_1"
+      dataSource={Banner01DataSource}
+      isMobile={isMobile}
+    />,
+    // <Content0
+    //   id="Content0_0"
+    //   key="Content0_0"
+    //   dataSource={Content00DataSource}
+    //   isMobile={isMobile}
+    // />,
+    <Feature2
+      id="Feature2_0"
+      key="Feature2_0"
+      dataSource={Feature20DataSource}
+      isMobile={isMobile}
+    />,
+    <Content3
+      id="Content3_0"
+      key="Content3_0"
+      dataSource={Content30DataSource}
+      isMobile={isMobile}
+    />,
+    <Feature7
+      id="Feature7_0"
+      key="Feature7_0"
+      dataSource={Feature70DataSource}
+      isMobile={isMobile}
+    />,
+    <Content5
+      id="Content5_0"
+      key="Content5_0"
+      dataSource={Content50DataSource}
+      isMobile={isMobile}
+    />,
+    <Footer1
+      id="Footer1_0"
+      key="Footer1_0"
+      dataSource={Footer10DataSource}
+      isMobile={isMobile}
+    />,
+  ];
+  return <div className="templates-wrapper">{children}</div>;
 }
