6033ec21633551fc772a4cf4d23eec860997abd7
Jaladh Singhal <jaladhsinghal@gmail.com>
2021-08-29 01:51:10 +0530

    Added input controls and connected it with orbit plot 3D

--- src/AnalysisTools/OrbitTool.jsx
+++ src/AnalysisTools/OrbitTool.jsx
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery, useQueries } from "react-query";
 import createPlotlyComponent from "react-plotly.js/factory";
 import Plotly from "plotly.js-dist-min";
+import { Select, DatePicker } from "antd";
+import moment from "moment";
+import momentTimezone from "moment-timezone";
 
 import { getData } from "../apiUtils";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 
+momentTimezone.tz.setDefault("Etc/UTC");
 const Plot = createPlotlyComponent(Plotly);
-const axisLayout = { exponentformat: "e", range: 1.15 };
+
+// TODO: Add expanded form in duration
+const trackLengths = [
+  { value: "24h", duration: [24, "h"] },
+  { value: "7d", duration: [7, "d"] },
+  { value: "15d", duration: [15, "d"] },
+  { value: "30d", duration: [30, "d"] },
+  { value: "60d", duration: [60, "d"] },
+  { value: "90d", duration: [90, "d"] },
+  { value: "4M", duration: [4, "M"] },
+  { value: "6M", duration: [6, "M"] },
+  { value: "1y", duration: [1, "y"] },
+];
+const timeSteps = ["12h", "24h", "48h", "7d", "10d", "15d", "30d", "60d"];
 
 export default function OrbitTool() {
+  // TODO: Add allBodies state
+  const [selectedBodies, setSelectedBodies] = useState([0, 1, 2]);
+  const [selectedTimeEnd, setSelectedTimeEnd] = useState(
+    moment().startOf("minute").valueOf() // current time with 0 s & 0 ms in UNIX timestamp
+  );
+  const [selectedTrackLength, setSelectedTrackLength] = useState(5);
+  const [selectedTimeStep, setSelectedTimeStep] = useState("12h");
+
   const bodiesQuery = useQuery(
     "orbit-tool-bodies",
     getData({ apiRoute: "/orbit-tool/bodies", isAnalysisTool: true }),
@@ -19,22 +44,97 @@ export default function OrbitTool() {
 
   if (bodiesQuery.isLoading) return <Loading />;
   if (bodiesQuery.error) return <Error response={bodiesQuery.error.response} />;
+
   return (
     <>
-      <OrbitToolInput bodies={bodiesQuery.data} />
-      <OrbitPlot3D bodies={bodiesQuery.data} />
+      <div>
+        <span>Bodies: </span>
+        <Select
+          mode="multiple"
+          placeholder="Please select bodies to plot"
+          defaultValue={selectedBodies}
+          style={{ width: 300 }}
+          onChange={(value) => {
+            setSelectedBodies(value);
+            console.log(value.map((i) => bodiesQuery.data[i].name));
+          }}
+        >
+          {bodiesQuery.data.map((body, i) => (
+            <Select.Option value={i}>{body.name}</Select.Option>
+          ))}
+        </Select>
+        <br />
+
+        <span>End Time (in UTC): </span>
+        <DatePicker
+          defaultValue={moment(selectedTimeEnd)}
+          allowClear={false}
+          showTime={{
+            format: "HH:mm",
+            minuteStep: 15,
+            defaultValue: moment("00:00:00", "HH:mm:ss"),
+          }}
+          onOk={(date) => {
+            setSelectedTimeEnd(date.valueOf());
+            console.log(date);
+          }}
+        />
+        <br />
+
+        <span>Length of track: </span>
+        <Select
+          defaultValue={selectedTrackLength}
+          style={{ width: 100 }}
+          onChange={(value) => {
+            setSelectedTrackLength(value);
+            console.log(trackLengths[value]);
+          }}
+        >
+          {trackLengths.map((trackLength, i) => (
+            <Select.Option value={i}>{trackLength.value}</Select.Option>
+          ))}
+        </Select>
+        <br />
+
+        <span>Time step: </span>
+        <Select
+          defaultValue={selectedTimeStep}
+          style={{ width: 100 }}
+          onChange={(value) => {
+            setSelectedTimeStep(value);
+            console.log(value);
+          }}
+        >
+          {timeSteps.map((timeStep) => (
+            <Select.Option value={timeStep}>{timeStep}</Select.Option>
+          ))}
+        </Select>
+      </div>
+      <OrbitPlot3D
+        bodies={selectedBodies.map((i) => bodiesQuery.data[i])}
+        timeStart={moment(selectedTimeEnd)
+          .subtract(...trackLengths[selectedTrackLength].duration)
+          .format("YYYY-MM-DDTHH:mm:ss")}
+        timeStop={moment(selectedTimeEnd).format("YYYY-MM-DDTHH:mm:ss")}
+        timeStep={selectedTimeStep}
+      />
     </>
   );
 }
 
-function OrbitPlot3D({ bodies }) {
+function OrbitPlot3D({ bodies, timeStart, timeStop, timeStep }) {
   const [plotLayout, setPlotLayout] = useState({
     width: 800,
     height: 600,
     title: "3D Orbit Tool",
-    xaxis: axisLayout,
-    yaxis: axisLayout,
-    zaxis: axisLayout,
+    xaxis: { exponentformat: "e" },
+    yaxis: { exponentformat: "e" },
+    zaxis: { exponentformat: "e" },
+    scene: {
+      xaxis: { range: [-1.15, 1.15] },
+      yaxis: { range: [-1.15, 1.15] },
+      zaxis: { range: [-1.15, 1.15] },
+    },
   });
 
   const [plotData, setPlotData] = useState([
@@ -49,37 +149,55 @@ function OrbitPlot3D({ bodies }) {
     },
   ]);
 
+  // add body's trace at index it is already present otherwise at end
+  function addBodyTrace(data) {
+    const newTrace = { ...data, type: "scatter3d", mode: "lines" };
+    // TODO: Add scatter trace for data[-1] with no legend, hover
+    setPlotData((prevPlotData) => {
+      // TODO: Maintain a hashmap of bodies-name and traceindex in plotData (-1 if not present)
+      const traceIndex = prevPlotData.findIndex(
+        (trace) => trace.name === newTrace.name
+      );
+      if (traceIndex >= 0) {
+        // if existing, replace the trace
+        const newPlotData = [...prevPlotData];
+        newPlotData[traceIndex] = newTrace;
+        return newPlotData;
+      } else return prevPlotData.concat(newTrace);
+    });
+  }
+
+  useEffect(() => {
+    setPlotData((prevPlotData) => {
+      const newPlotData = prevPlotData.filter(
+        (trace) =>
+          trace.name === "Sun" ||
+          bodies.findIndex((body) => body.name === trace.name) >= 0 // trace is for some body
+      );
+      console.log(newPlotData);
+      return newPlotData;
+    });
+  }, [bodies]);
+
   useQueries(
     bodies.map((body) => {
       return {
-        queryKey: ["orbitData", body.name],
+        queryKey: ["orbitData", body.name, timeStart, timeStop, timeStep],
         queryFn: getData({
           apiRoute: "/orbit-tool",
           getParams: {
-            timeStart: "2021-01-06T00:00:00",
-            timeStop: "2022-01-06T00:00:00",
-            timeStep: "2d",
+            timeStart: timeStart,
+            timeStop: timeStop,
+            timeStep: timeStep,
             body: body.body_id,
           },
           isAnalysisTool: true,
         }),
-        onSuccess: (data) => {
-          setPlotData(
-            plotData.concat({
-              ...data,
-              type: "scatter3d",
-              mode: "lines",
-            })
-          );
-        },
+        onSuccess: (bodyOrbitData) => addBodyTrace(bodyOrbitData),
         refetchOnWindowFocus: false,
       };
     })
   );
 
-  return <Plot data={plotData} layout={plotLayout} />;
-}
-
-function OrbitToolInput({ bodies }) {
-  return null;
+  return <Plot divId="orbitPlot3D" data={plotData} layout={plotLayout} />;
 }
--- src/AnalysisTools/index.jsx
+++ src/AnalysisTools/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import OrbitTool from "./OrbitTool";
+import React, { lazy, Suspense } from "react";
+import Loading from "../components/Loading";
+
+const OrbitTool = lazy(() => import("./OrbitTool")); // for code splitting
 
 export default function AnalysisTool({ toolName }) {
-  if (toolName === "orbit_tool") return <OrbitTool />;
+  if (toolName === "orbit_tool")
+    return (
+      <Suspense fallback={<Loading />}>
+        <OrbitTool />
+      </Suspense>
+    );
   else return null;
 }
